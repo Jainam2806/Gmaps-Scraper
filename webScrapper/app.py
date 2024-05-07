9f698667ce569575404c6ec3a4fb3ac8bf325b89
Jainam Parekh <jainamp0005@gmail.com>
2024-05-07 21:46:11 +0530

    host changed

--- webScrapper/app.py
+++ webScrapper/app.py
@@ -49,4 +49,4 @@ def download():
     return send_file(return_data, mimetype='text/csv',as_attachment=True,download_name=csv_filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    app.run(debug=True, host='0.0.0.0' ,port=10000)
